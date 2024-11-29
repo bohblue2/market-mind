d5c590b3e3b590a547d3de01692985eebc38715a
BrianBae <brian.bae.contact@gmail.com>
2024-11-29 20:28:02 +0900

    feat: remove /api/chat

--- mm_backend/routers/api.py
+++ mm_backend/routers/api.py
@@ -1,11 +1,10 @@
 from fastapi import APIRouter
 
-from .v1 import chat, root, securities
+from .v1 import root, securities
 
 router = APIRouter(
     prefix="/api",
 )
 
-router.include_router(chat.router)
 router.include_router(root.router)
 router.include_router(securities.router)
